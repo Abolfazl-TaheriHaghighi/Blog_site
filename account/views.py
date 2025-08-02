0ae594d2809e191c94c7512115775f8cb83c966e
Abolfazl-TaheriHaghighi <atahry6@gmail.com>
2025-08-02 17:50:52 +0330

    add backend logout and register index , add Dynamic urls

--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def user_login (request):
 
     if request.user.is_authenticated == True:
-        return redirect('/')
+        return redirect('home:main')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -14,17 +15,34 @@ def user_login (request):
         user = authenticate(request , username = username , password = password)
         if user is not None:
             login(request , user)
-            return redirect('/')
+            return redirect('home:main')
 
     return render(request , "account/login.html" , {})
 
 
 
 def user_register (request):
+    if request.user.is_authenticated == True:
+        return redirect('home:main')
+
+    context = {'errors' : []}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('pass1')
+        password2 = request.POST.get('pass2')
+        if password1 != password2:
+            context['errors'].append('passwords are not the same')
+            return render(request, 'account/register.html', context)
+
+        user = User.objects.create(username= username , password= password1 , email= email)
+        login(request,user)
+        return redirect('home:main')
     return render(request , 'account/register.html' , {})
 
 
 
 def user_logout (request):
     logout(request)
-    return redirect("/")
+    return redirect('home:main')
