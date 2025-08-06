9bf8a6f5bc97bb6356fab51cd4b89bdb9021de85
Abolfazl-TaheriHaghighi <atahry6@gmail.com>
2025-08-06 17:27:07 +0330

    Dynamicizing article(Post) categories

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,13 @@ from django.utils import timezone
 class Categories (models.Model):
     title = models.CharField(max_length= 100)
     created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
 
 class Post (models.Model):
 
     author = models.ForeignKey(User , on_delete= models.CASCADE)
-    Category = models.ManyToManyField(Categories)
+    category = models.ManyToManyField(Categories)
     title = models.CharField(max_length=80)
     body = models.TextField()
     image = models.ImageField(upload_to= 'images/post')
