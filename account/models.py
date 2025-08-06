0a89e2432a1c12b190766a0833214340b292f163
Abolfazl-TaheriHaghighi <atahry6@gmail.com>
2025-08-06 17:56:11 +0330

    creat Profile models and fix type models for phone

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User , on_delete= models.CASCADE)
+    phone = models.CharField(max_length=15)
+    id_card = models.IntegerField(max_length=15)
+    image = models.ImageField(upload_to="Profile/images" , blank=True , null=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.user.username}"
