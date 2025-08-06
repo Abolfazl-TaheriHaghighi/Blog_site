905ab532ac4d6cf80c4247f3ccfaec37f0dcf8f2
Abolfazl-TaheriHaghighi <atahry6@gmail.com>
2025-08-06 17:02:19 +0330

    add Categories in models.py from blog app

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,20 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+class Categories (models.Model):
+    title = models.CharField(max_length= 100)
+    created = models.DateTimeField(auto_now_add=True)
+
 class Post (models.Model):
 
     author = models.ForeignKey(User , on_delete= models.CASCADE)
+    Category = models.ManyToManyField(Categories)
     title = models.CharField(max_length=80)
     body = models.TextField()
     image = models.ImageField(upload_to= 'images/post')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    pub_date = models.DateField(default=timezone.now())
+    pub_date = models.DateField(default=timezone.now)
 
 
     def __str__(self):
